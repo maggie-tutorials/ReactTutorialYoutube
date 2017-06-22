020673b3cc72c6a4000a25f7a3cb3aae5ed91e92
MaggieSerino <m.serino@hotmail.it>
2017-06-22 15:48:37 +0200

    Adding state to SearchBar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,9 +4,37 @@ import React, { Component } from 'react';
 // Defines a new class SearchBar that has access to all the functionality
 // that React.Component has.
 class SearchBar extends Component {
+  // State is a plain JS object that is used to record, and react, to user events.
+  // Each class-based component that we define has its own state object.
+  // Whenever the state is changed, the component immediately re-renders.
+  // and forces its children to re-render.
+  // Only class-based components have state.
+  // The constructor function is the only one that gets called automatically
+  // whenever the component is created.
+  constructor(props) {
+    // Calling the parent method
+    super(props);
+
+    this.state = {term: ''};
+  }
+
   // Every class-based React component needs to have a render method
   render() {
-    return <input />;
+    return (
+      <div>
+        // A controlled field is an element whose value is set by the state
+        <input
+        value={this.state.term}
+        onChange={event => this.setState({ term: event.target.value })}/>
+      </div>
+    )
+  }
+
+  // Event handler is a function that runs whenever the event occurs.
+  // All browser events come with an event payload that describes the context
+  // in which the event has occurred.
+  onInputChange(event) {
+      console.log(event.target.value);
   }
 }
 
