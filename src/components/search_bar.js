c410e3ec83255ef370fa3ecfda49123ca6615fa4
MaggieSerino <m.serino@hotmail.it>
2017-06-22 15:18:11 +0200

    Adding SearchBar component

---
+++ src/components/search_bar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+// ES6 Class is a Javascript object that holds properties and methods
+// Defines a new class SearchBar that has access to all the functionality
+// that React.Component has.
+class SearchBar extends Component {
+  // Every class-based React component needs to have a render method
+  render() {
+    return <input />;
+  }
+}
+
+export default SearchBar;
