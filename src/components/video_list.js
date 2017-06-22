187decbd0c36e66a781278dd7a899ebff55fd2b9
MaggieSerino <m.serino@hotmail.it>
2017-06-22 17:41:15 +0200

    Displaying results from VideoListItem component

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 // In a functional component, props are passed as arguments
 // In a class-based component, props are available anywhere as this.props
 const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem key={ video.etag } video={ video } />;
+  });
+
   return (
     <ul className="col-md-4 list-group">
-      { props.videos.length }
+      { videoItems }
     </ul>
   );
 };
---
+++ src/components/video_list_item.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const VideoListItem = ( { video }) => {
+  // doing { video } is the same as const video = props.video
+  const imgUrl = video.snippet.thumbnails.default.url;
+
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img clasName="media-object" src={ imgUrl } />
+        </div>
+        <div className="media-body">
+          <div className="media-heading">{ video.snippet.title }</div>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default VideoListItem;
